0cf55dcc7fa45fbb189dd55e3aaee64ae70d2a52
Tony Ye <tony.zh.ye@gmail.com>
2019-01-03 14:01:32 +0800

    With bootstrap grid

--- src/components/site/site-nav/index.tsx
+++ src/components/site/site-nav/index.tsx
@@ -7,8 +7,16 @@ interface Props {}
 export const SiteNav: SFC<Props> = (props) => {
   return (
     <nav className="top-nav">
-      <a className="navbar-brand" href="#">Kanban for One</a>
-      {props.children}
+      <div className="container-fluid no-gutters">
+        <div className="row justify-content-between">
+          <div className="col-8 col-md-6">
+            <a className="navbar-brand" href="#">Kanban for One</a>
+          </div>
+          <div className="col-4 col-md-2">
+            {props.children}
+          </div>
+        </div>
+      </div>
     </nav>
   );
 };
